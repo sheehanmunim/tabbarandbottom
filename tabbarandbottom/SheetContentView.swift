f1ee2447bfac6275e597d14b712948619daef440
sheehanmunim <sheehanmunim@gmail.com>
2024-06-06 19:08:08 -0400

    working with dragging bottomsheet

---
+++ tabbarandbottom/SheetContentView.swift
@@ -0,0 +1,18 @@
+//
+//  SheetContentView.swift
+//  tabbarandbottom
+//
+//  Created by Sheehan Munim on 6/6/24.
+//
+
+import SwiftUI
+
+struct SheetContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SheetContentView()
+}
