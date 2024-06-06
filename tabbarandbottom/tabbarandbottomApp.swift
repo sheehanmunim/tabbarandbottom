e1cddd2161b57c7e43c3d5e72573cf33c0122ca1
sheehanmunim <sheehanmunim@gmail.com>
2024-06-06 17:17:14 -0400

    Initial Commit

---
+++ tabbarandbottom/tabbarandbottomApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabbarandbottomApp.swift
+//  tabbarandbottom
+//
+//  Created by Sheehan Munim on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct tabbarandbottomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
