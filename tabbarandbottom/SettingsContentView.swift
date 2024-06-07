253a710c6c83d0db17b634cc104d52ca934df38f
sheehanmunim <sheehanmunim@gmail.com>
2024-06-07 03:57:56 -0400

    With tab bar bult into bottom sheet, but works

---
+++ tabbarandbottom/SettingsContentView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsContentView.swift
+//  tabbarandbottom
+//
+//  Created by Sheehan Munim on 6/7/24.
+//
+
+import SwiftUI
+
+struct SettingsContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsContentView()
+}
