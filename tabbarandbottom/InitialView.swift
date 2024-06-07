3afdd39cfe729d535bbe748dd8bcd03e75e17db8
sheehanmunim <sheehanmunim@gmail.com>
2024-06-07 19:50:37 -0400

    this is naivagtion to the tab bar attached to the bottom sheet

---
+++ tabbarandbottom/InitialView.swift
@@ -0,0 +1,18 @@
+//
+//  InitialView.swift
+//  tabbarandbottom
+//
+//  Created by Sheehan Munim on 6/7/24.
+//
+
+import SwiftUI
+
+struct InitialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InitialView()
+}
